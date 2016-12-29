f535578f1c81188a8f8d2d214b914e862c7f9ff7
iamtanveer <tanveer7537@gmail.com>
2016-12-29 13:19:52 +0530

    Add files via upload
    made UI for Vendor side

---
+++ Beaconizer(Business)/Beaconizer(Business)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Beaconizer(Business)
+//
+//  Created by Gagandeep Nagpal on 29/12/16.
+//  Copyright © 2016 Gagandeep Nagpal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
